0f834a948d3565a7c05da44eb565afcd1a777691
Setyo <hsetyobudi@gmail.com>
2021-11-11 17:11:39 +0100

    Getting lost :)
    Problème de transfert vers localstorage

--- P5-Dev-Web-Kanap-master/front/js/product.js
+++ P5-Dev-Web-Kanap-master/front/js/product.js
@@ -1,7 +1,31 @@
 let search_params = new URLSearchParams(window.location.search);
 let id = search_params.get('id');
-
 let urlProduct = (`http://localhost:3000/api/products/${id}`);
+let article = "";
+
+const colorChoice = document.querySelector('#colors');
+const quantity = document.querySelector('#quantity');
+
+getArticle();
+
+function getArticle() {
+    fetch(urlProduct)
+        .then((res) => {
+            return res.json();
+        })
+        .then(async function (resultAPI) {
+            article = await resultAPI;
+            console.log(article);
+            if (article) {
+                getPost(article);
+            }
+        })
+        .catch((error) => {
+            console.log("Erreur requete API")
+        })
+}
+
+// fonction pour afficher l'image sur la page produit*****
 
 fetch(urlProduct).then((response) =>
     response.json()
@@ -13,6 +37,7 @@ fetch(urlProduct).then((response) =>
             let name = data.name;
             let price = data.price;
             let idProduct = data.id;
+
 
             document.querySelector('#title').innerHTML = name;
             document.querySelector('#price').innerHTML = price;
@@ -29,68 +54,171 @@ fetch(urlProduct).then((response) =>
                 console.log(structureOptions);
             }
 
-            const choixcouleur = document.querySelector("#colors");
-            choixcouleur.innerHTML = structureOptions;
+            const selectionCouleur = document.querySelector("#colors");
+            selectionCouleur.innerHTML = structureOptions;
+        })
 
+,)
+function addProductLocalStorage(article) {
+    const button = document.querySelector('#addToCart');
+    button.addEventListener("click", function () {
+        if (quantity.value > 0 && quantity.value <= 100 && quantity.value != 0) {
+            let choixCouleur = colorChoice;
+            let nameProduct = data.name;
+            let prix = data.price;
+            let idKanap = data.id;
+            let imageKanap = data.imageUrl;
+            let altTxt = data.altTxt;
+            let description = data.description;
+            let colorSelection = choixCouleur;
+            let quantite = data.quantity;
+
+
+            let optionsProduct = {
+                choixCouleur,
+                nameProduct,
+                prix,
+                idKanap,
+                imageKanap,
+                altTxt,
+                description,
+                colorSelection,
+                quantite
+
+            };
+
+            // Initialisation du local Storage****
+            let productInLocalStorage = JSON.parse(localStorage.getItem("product"));
+
+            if (productInLocalStorage) {
+                const resultFind = productInLocalStorage.find(
+                    (el) => el.idKanap === idKanap && el.colorSelection === choixCouleur);
+                if (resultFind) {
+                    let newQuantity =
+                        parseInt(optionsProduct.quantite) + parseInt(resultFind.quantite);
+                    resultFind.quantite = newQuantity;
+                    localStorage.setItem("product", JSON.stringify(productInLocalStorage));
+                    console.log(productInLocalStorage);
+                }
+            }
+            else {
+                productInLocalStorage.push(optionsProduct);
+                localStorage.setItem("product", JSON.stringify(productInLocalStorage));
+                console.log(productInLocalStorage);
+            }
+        }
+        else {
+            productInLocalStorage = [];
+            productInLocalStorage.push(optionsProduct);
+            localStorage.setItem("product", JSON.stringify(productInLocalStorage));
+            console.log(productInLocalStorage);
+        }
+
+    })
+}
+;;
+
+
+
+/*
+fetch(urlProduct).then((response) =>
+    response.json()
+        .then((data) => {
+            const affichageItemImg = document.querySelector('.item__img');
+            const affichagePhoto = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
+            affichageItemImg.insertAdjacentHTML('afterbegin', affichagePhoto);
+
+            let name = data.name;
+            let price = data.price;
+            let idProduct = data.id;
+
+
+            document.querySelector('#title').innerHTML = name;
+            document.querySelector('#price').innerHTML = price;
+            document.querySelector('#description').innerHTML = data.description;
+
+            const color = data.colors;
+            const optioncolor = color;
+            let structureOptions = [];
+
+            for (let i = 0; i < color.length; i++) {
+                structureOptions += `
+            <option>${color[i]}</option>
+            `;
+                console.log(structureOptions);
+            }
+
+            const selectionCouleur = document.querySelector("#colors");
+            selectionCouleur.innerHTML = structureOptions;
+        })
+
+        // fonction pour ajouter les donnees au local storage****
+
+        .then((data) => {
             const button = document.querySelector('#addToCart');
             button.addEventListener("click", function () {
+                function addProductLocalStorage() {
+                    const button = document.querySelector('#addToCart');
+                    button.addEventListener("click", function () {
+                        const colorChoice = document.querySelector('#colors').value;
+                        const quantity = document.querySelector('#quantity').value;
 
-                const colorChoice = document.querySelector('#colors').value;
-                const quantity = document.querySelector('#quantity').value;
+                        if (quantity.value > 0 && quantity.value <= 100 && quantity.value != 0) {
 
-                // A mettre dans conditions ..... else
+                            let choixCouleur = colorChoice;
+                            let nameProduct = data.name;
+                            let prix = data.price;
+                            let idKanap = data.id;
+                            let imageKanap = data.imageUrl;
+                            let altTxt = data.altTxt;
+                            let description = data.description;
+                            let colorSelection = choixCouleur;
+                            let quantite = data.quantity;
 
-                /*function argumentContientColor() {
-                    return [].includes.call(arguments, 'colors');
+
+                            let optionsProduct = {
+                                choixCouleur,
+                                nameProduct,
+                                prix,
+                                idKanap,
+                                imageKanap,
+                                altTxt,
+                                description,
+                                colorSelection,
+                                quantite
+
+                            };
+
+                            // Initialisation du local Storage****
+                            let productInLocalStorage = JSON.parse(localStorage.getItem("product"));
+
+                            if (productInLocalStorage) {
+                                const resultFind = productInLocalStorage.find(
+                                    (el) => el.idKanap === idKanap && el.colorSelection === choixCouleur);
+                                if (resultFind) {
+                                    let newQuantity =
+                                        parseInt(optionsProduct.quantite) + parseInt(resultFind.quantite);
+                                    resultFind.quantite = newQuantity;
+                                    localStorage.setItem("product", JSON.stringify(productInLocalStorage));
+                                    console.log(productInLocalStorage);
+                                }
+                            }
+                            else {
+                                productInLocalStorage.push(optionsProduct);
+                                localStorage.setItem("product", JSON.stringify(productInLocalStorage));
+                                console.log(productInLocalStorage);
+                            }
+                        }
+                        else {
+                            productInLocalStorage = [];
+                            productInLocalStorage.push(optionsProduct);
+                            localStorage.setItem("product", JSON.stringify(productInLocalStorage));
+                            console.log(productInLocalStorage);
+                        }
+
+                    })
                 }
-                console.log(argumentContientColor('color'));
-                */
-
-                //if (localStorage.getItem("products")) {
-                //  productInLocalStorage = JSON.parse(localStorage.getItem("products"));
-                //const idAlreadySelected = productInLocalStorage.filter(products => products.idProduct === products.id);
-                //const colorAlreadySelected = productInLocalStorage.filter(products => products.colorChoice === products.colorChoice);
-                //}
-
-                //for (let product of products) {
-                ///  if (product.colorChoice === colorAlreadySelected.products.colorChoice && products.id === idAlreadySelected.products.id) {
-                //product.quantityChoice = total;
-                //}
-                //}
-
-
-
-
-                let optionsProduct = {
-                    idProduct, quantity, colorChoice, name, price, affichagePhoto
-                }
-
-                let productInLocalStorage = JSON.parse(localStorage.getItem("product"));
-
-                const addProductLocalStorage = () => {
-                    //if (idProduct === idProduct && colorChoice === colorChoice) {
-                    //  quantity = total;
-                    //console.log(total);
-                    //}
-                    //else {
-
-
-                    productInLocalStorage.push(optionsProduct);
-                    localStorage.setItem("product", JSON.stringify(productInLocalStorage));
-                    //}
-                };
-
-                if (productInLocalStorage) {
-                    addProductLocalStorage();
-
-                } else {
-                    productInLocalStorage = [];
-                    addProductLocalStorage();
-                }
-
             });
-        })
-);
+        }));
 
-
-
+*/
