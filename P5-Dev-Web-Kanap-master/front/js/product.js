d049a3933a219576633ea406ebb7e8688f16507a
Setyo <hsetyobudi@gmail.com>
2021-11-21 11:07:55 +0100

    Return to basic

--- P5-Dev-Web-Kanap-master/front/js/product.js
+++ P5-Dev-Web-Kanap-master/front/js/product.js
@@ -1,102 +1,8 @@
 let search_params = new URLSearchParams(window.location.search);
 let id = search_params.get('id');
-console.log(id);
+
 let urlProduct = (`http://localhost:3000/api/products/${id}`);
-let article = "";
 
-productInLocalStorage = [];
-localStorage.setItem("product", JSON.stringify(productInLocalStorage));
-
-
-
-
-
-
-// fonction pour afficher l'image sur la page produit*****
-
-fetch(urlProduct).then((response) =>
-    response.json()
-        .then((data) => {
-            console.log(data);
-            const affichageItemImg = document.querySelector('.item__img');
-            const affichagePhoto = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
-            affichageItemImg.insertAdjacentHTML('afterbegin', affichagePhoto);
-
-            let name = data.name;
-            let price = data.price;
-            let idProduct = data._id;
-
-
-            document.querySelector('#title').innerHTML = name;
-            document.querySelector('#price').innerHTML = price;
-            document.querySelector('#description').innerHTML = data.description;
-
-            const color = data.colors;
-            //const optioncolor = color;
-            let structureOptions = [];
-
-            for (let i = 0; i < color.length; i++) {
-                structureOptions += `
-            <option>${color[i]}</option>
-            `;
-
-            }
-
-            const colorChoice = document.querySelector('#colors');
-            const quantity = document.querySelector('#quantity');
-            colorChoice.innerHTML = structureOptions;
-
-            const button = document.querySelector('#addToCart');
-            button.addEventListener("click", function () {
-
-                if (quantity > 0 && quantity <= 100 && quantity != 0) {
-
-                    let optionsProduct = {
-                        choixCouleur: colorChoice,
-                        nameProduct: data.name,
-                        prix: data.price,
-                        idKanap: data._id,
-                        imageKanap: data.imageUrl,
-                        altTxt: data.altTxt,
-                        description: data.description,
-                        quantity,
-                    };
-                    console.log(optionsProduct);
-
-                    // Initialisation du local Storage****
-                    let productInLocalStorage = JSON.parse(localStorage.getItem("product"));
-
-                    if (productInLocalStorage) {
-
-                        const resultFind = productInLocalStorage.find(
-                            (el) => el.idKanap === optionProduct.idKanap && el.colorChoice === optionProduct.choixCouleur);
-                        if (resultFind) {
-                            console.log(resultFind);
-                            let newQuantity =
-                                parseInt(optionsProduct.quantite) + parseInt(resultFind.quantite);
-
-                            resultFind.quantite = newQuantity;
-                            localStorage.setItem("product", JSON.stringify(productInLocalStorage));
-
-                        }
-                        else {
-                            productInLocalStorage.push(optionsProduct);
-                            localStorage.setItem("product", JSON.stringify(productInLocalStorage));
-
-
-                        }
-                    }
-                }
-            })
-
-        })
-
-    ,)
-
-
-
-
-/*
 fetch(urlProduct).then((response) =>
     response.json()
         .then((data) => {
@@ -107,7 +13,6 @@ fetch(urlProduct).then((response) =>
             let name = data.name;
             let price = data.price;
             let idProduct = data.id;
-
 
             document.querySelector('#title').innerHTML = name;
             document.querySelector('#price').innerHTML = price;
@@ -124,77 +29,65 @@ fetch(urlProduct).then((response) =>
                 console.log(structureOptions);
             }
 
-            const selectionCouleur = document.querySelector("#colors");
-            selectionCouleur.innerHTML = structureOptions;
-        })
+            const choixcouleur = document.querySelector("#colors");
+            choixcouleur.innerHTML = structureOptions;
 
-        // fonction pour ajouter les donnees au local storage****
-
-        .then((data) => {
             const button = document.querySelector('#addToCart');
             button.addEventListener("click", function () {
-                function addProductLocalStorage() {
-                    const button = document.querySelector('#addToCart');
-                    button.addEventListener("click", function () {
-                        const colorChoice = document.querySelector('#colors').value;
-                        const quantity = document.querySelector('#quantity').value;
 
-                        if (quantity.value > 0 && quantity.value <= 100 && quantity.value != 0) {
+                const colorChoice = document.querySelector('#colors').value;
+                const quantity = document.querySelector('#quantity').value;
 
-                            let choixCouleur = colorChoice;
-                            let nameProduct = data.name;
-                            let prix = data.price;
-                            let idKanap = data.id;
-                            let imageKanap = data.imageUrl;
-                            let altTxt = data.altTxt;
-                            let description = data.description;
-                            let colorSelection = choixCouleur;
-                            let quantite = data.quantity;
+                // A mettre dans conditions ..... else
 
-
-                            let optionsProduct = {
-                                choixCouleur,
-                                nameProduct,
-                                prix,
-                                idKanap,
-                                imageKanap,
-                                altTxt,
-                                description,
-                                colorSelection,
-                                quantite
-
-                            };
-
-                            // Initialisation du local Storage****
-                            let productInLocalStorage = JSON.parse(localStorage.getItem("product"));
-
-                            if (productInLocalStorage) {
-                                const resultFind = productInLocalStorage.find(
-                                    (el) => el.idKanap === idKanap && el.colorSelection === choixCouleur);
-                                if (resultFind) {
-                                    let newQuantity =
-                                        parseInt(optionsProduct.quantite) + parseInt(resultFind.quantite);
-                                    resultFind.quantite = newQuantity;
-                                    localStorage.setItem("product", JSON.stringify(productInLocalStorage));
-                                    console.log(productInLocalStorage);
-                                }
-                            }
-                            else {
-                                productInLocalStorage.push(optionsProduct);
-                                localStorage.setItem("product", JSON.stringify(productInLocalStorage));
-                                console.log(productInLocalStorage);
-                            }
-                        }
-                        else {
-                            productInLocalStorage = [];
-                            productInLocalStorage.push(optionsProduct);
-                            localStorage.setItem("product", JSON.stringify(productInLocalStorage));
-                            console.log(productInLocalStorage);
-                        }
-
-                    })
+                /*function argumentContientColor() {
+                    return [].includes.call(arguments, 'colors');
                 }
-            });
-        }));
+                console.log(argumentContientColor('color'));
+                */
 
-*/
+                //if (localStorage.getItem("products")) {
+                //  productInLocalStorage = JSON.parse(localStorage.getItem("products"));
+                //const idAlreadySelected = productInLocalStorage.filter(products => products.idProduct === products.id);
+                //const colorAlreadySelected = productInLocalStorage.filter(products => products.colorChoice === products.colorChoice);
+                //}
+
+                //for (let product of products) {
+                ///  if (product.colorChoice === colorAlreadySelected.products.colorChoice && products.id === idAlreadySelected.products.id) {
+                //product.quantityChoice = total;
+                //}
+                //}
+
+
+
+
+                let optionsProduct = {
+                    idProduct, quantity, colorChoice, name, price, affichagePhoto
+                }
+
+                let productInLocalStorage = JSON.parse(localStorage.getItem("product"));
+
+                const addProductLocalStorage = () => {
+                    //if (idProduct === idProduct && colorChoice === colorChoice) {
+                    //  quantity = total;
+                    //console.log(total);
+                    //}
+                    //else {
+
+
+                    productInLocalStorage.push(optionsProduct);
+                    localStorage.setItem("product", JSON.stringify(productInLocalStorage));
+                    //}
+                };
+
+                if (productInLocalStorage) {
+                    addProductLocalStorage();
+
+                } else {
+                    productInLocalStorage = [];
+                    addProductLocalStorage();
+                }
+
+            });
+        })
+);
