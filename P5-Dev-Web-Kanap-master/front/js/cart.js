ee537c8ba6eb3b4104d293e3cdff0c37b3a0cd94
Setyo <hsetyobudi@gmail.com>
2021-12-01 22:04:53 +0100

    Recherche formulaire

--- P5-Dev-Web-Kanap-master/front/js/cart.js
+++ P5-Dev-Web-Kanap-master/front/js/cart.js
@@ -23,7 +23,7 @@ else {
 
   for (let product of productInLocalStorage) {
     structureProductsCart.innerHTML += `
-    <article class="cart__item" data-id="{product-ID}">
+    <article class="cart__item" data-id="${product._id}">
     <div class="cart__item__img">
       <img src= ${product.image} alt= ${product.altTxt}>
     </div>
@@ -59,12 +59,12 @@ else {
 
 // créer la variable pour mettre le prix total du panier
 
-
+/*
 function modifQuantity() {
-  let itemModifQuantity = document.querySelector(".itemQuantity");
+  const itemModifQuantity = document.querySelectorAll(".itemQuantity");
 
-  for (let q = 0; q < itemModifQuantity.length; q++) {
-    itemModifQuantity[q].addEvenlistener("change", (event) => {
+  for (let itemModifQuantity of productInLocalStorage) {
+    itemModifQuantity.addEvenlistener('change', (event) => {
       event.preventDefault();
 
       let quantityModif = productInLocalStorage[q].quantity;
@@ -73,15 +73,43 @@ function modifQuantity() {
       const resultFind = productInLocalStorage.find((el) => el.quantityModifValue !== quantityModif);
 
       resultFind.quantity = quantityModifValue;
-      productInLocalStorage[q].quantity = resultFind.quantity;
+      productInLocalStorage[k].quantity = resultFind.quantity;
 
-      localStorage.setItem("products", JSON.stringify(productInLocalStorage));
+      localStorage.setItem("product", JSON.stringify(productInLocalStorage));
 
       location.reload();
     })
   }
 }
 modifQuantity();
+
+*/
+
+
+// total quantite dans le panier 
+
+let totalQuantityCalcule = [];
+
+//Chercher la quantite dans le panier
+
+for (let n = 0; n < productInLocalStorage.length; n++) {
+  let quantityOfProductsInCartString = productInLocalStorage[n].quantity;
+  let quantityOfProductsInCart = parseInt(quantityOfProductsInCartString);
+  //total quantite dans le panier
+  totalQuantityCalcule.push(quantityOfProductsInCart)
+  console.log(totalQuantityCalcule);
+
+  // additionner la quantite dans le panier
+  function reducer(accumulator, currentValue) {
+    return accumulator + currentValue;
+  }
+  const totalQuantityProducts = totalQuantityCalcule.reduce(reducer);
+  console.log(totalQuantityProducts);
+
+  let totalQuantityEnd = document.getElementById("totalQuantity");
+  totalQuantityEnd.innerHTML = totalQuantityProducts;
+
+}
 
 
 
@@ -115,42 +143,91 @@ for (let m = 0; m < productInLocalStorage.length; m++) {
 
 }
 
-// total quantite dans le panier 
-
-let totalQuantityCalcule = [];
-
-//Chercher la quantite dans le panier
-
-for (let n = 0; n < productInLocalStorage.length; n++) {
-  let quantityOfProductsInCartString = productInLocalStorage[n].quantity;
-  let quantityOfProductsInCart = parseInt(quantityOfProductsInCartString);
-  //total quantite dans le panier
-  totalQuantityCalcule.push(quantityOfProductsInCart)
-  console.log(totalQuantityCalcule);
-
-  // additionner la quantite dans le panier
-  function reducer(accumulator, currentValue) {
-    return accumulator + currentValue;
-  }
-  const totalQuantityProducts = totalQuantityCalcule.reduce(reducer);
-  console.log(totalQuantityProducts);
-
-  let totalQuantityEnd = document.getElementById("totalQuantity");
-  totalQuantityEnd.innerHTML = totalQuantityProducts;
-
-}
-
-
-
-
 //**************** Formulaire*****************
 
 // AddEventlistener****
 // Selectionner le bouton pour envoyer le formulaire****
 
-//const btnSendFormOrder = document.querySelector("#order");
-//console.log(btnSendFormOrder);
+const btnSendFormOrder = document.getElementById("order");
+console.log(btnSendFormOrder);
 
+btnSendFormOrder.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  //Recuperation des valeurs du formulaire
+
+  const formulaireValues = {
+    prenom: document.getElementById("firstName").value,
+    nom: document.getElementById("lastName").value,
+    adresse: document.getElementById("address").value,
+    ville: document.getElementById("city").value,
+    email: document.getElementById("email").value
+  }
+
+  console.log("formulaireValues");
+  console.log(formulaireValues);
+  //Mettre l'objet "formulaireValues" dans le localStorage 
+  localStorage.setItem("formulaireValues", JSON.stringify(formulaireValues));
+
+
+
+  //récupération des valeurs du formulaire pour les mettre dans le localstorage
+  //localStorage.setItem("firstName", document.getElementById("firstName").value);
+  //console.log(document.getElementById("firstName").value);
+  //localStorage.setItem("lastName", document.getElementById("lastName").value);
+  //localStorage.setItem("address", document.getElementById("address").value);
+  //localStorage.setItem("city", document.getElementById("city").value);
+  //localStorage.setItem("email", document.getElementById("email").value);
+
+  //Mettre les values du formulaire dans un objet
+  //const formulaire = {
+  //prenom: localStorage.getItem("firstName"),
+  //nom: localStorage.getItem("lastName"),
+  //adresse: localStorage.getItem("address"),
+  //ville: localStorage.getItem("city"),
+  //email: localStorage.getItem("email"),
+  //}
+  //console.log("formulaire");
+  //console.log(formulaire);
+
+  //Mettre le contenu du formulaire et le produit selectionné dans un objet à envoyer au serveur
+
+  const aEnvoyer = {
+    productInLocalStorage,
+    formulaireValues
+  }
+
+  console.log("aEnvoyer");
+  console.log(aEnvoyer);
+})
+
+//---------Mettre le contenu du localstorage dans le formulaire------
+// Prendre la key du local storage et la mettre dans un variable
+const dataLocalStorage = localStorage.getItem("formulaireValues");
+
+//Convertir la chaine de caractère en objet javascript
+
+const dataLocalStorageObjet = JSON.parse(dataLocalStorage);
+
+console.log("dataLocalStorageObjet");
+console.log(dataLocalStorageObjet);
+
+//Mettre les values du localstorage dans les champs du formulaire
+document.getElementById("firstName").value = dataLocalStorageObjet.prenom;
+document.getElementById("lastName").value = dataLocalStorageObjet.nom;
+document.getElementById("address").value = dataLocalStorageObjet.adresse;
+document.getElementById("city").value = dataLocalStorageObjet.ville;
+document.getElementById("email").value = dataLocalStorageObjet.email;
+
+
+
+
+
+
+
+
+
+/*
 const contact = {
   firstName: "Heri",
   lastName: "Setyobudi",
@@ -168,7 +245,7 @@ products.push("chat")
 console.log(products);
 products.push("chien");
 console.log(products);
-
+*/
 
 //btnSendFormOrder.addEventListener("click", (e) => {
   //e.preventDefault();
